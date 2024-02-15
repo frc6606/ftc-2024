53253e005df5c062527a161bc6b8a64cf9437a0f
Vanhermo <vanessahermosillod@gmail.com>
2024-02-15 15:22:56 -0600

    feat: add autos with webcam

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutos.java
@@ -0,0 +1,231 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.teamcode.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.subsystems.Claw;
+import org.firstinspires.ftc.teamcode.subsystems.Drone;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import org.pinkhawks.ftc.drive.PoseStorage;
+import org.pinkhawks.ftc.drive.SampleMecanumDrive;
+import org.pinkhawks.ftc.subsystems.SubsystemManager;
+
+import java.util.List;
+
+@Autonomous(name="RedAutos", group="Auto")
+public class RedAutos extends LinearOpMode {
+    private final SubsystemManager subsystemManager = new SubsystemManager();
+    private ElapsedTime runtime = new ElapsedTime();
+    private static  final boolean USE_WEBCAM = true;
+    private static final String TFOD_MODEL_ASSET = "model_20240211_085857.tflite";
+    private static final String[] LABELS = {
+        "REDTP",
+    };
+    private TfodProcessor tfod;
+    private VisionPortal visionPortal;
+    private Arm arm = null;
+    private Claw claw = null;
+    private Drone drone = null;
+    private Trajectory leftTraj0, leftTraj1, leftTraj2, leftTraj3, leftTraj4, leftTraj5, leftTraj6;
+    private Trajectory centerTraj0, centerTraj1, centerTraj2, centerTraj3, centerTraj4;
+    private Trajectory rightTraj0, rightTraj1, rightTraj2, rightTraj3, rightTraj4, rightTraj5, rightTraj6;
+
+    private void initTfod() {
+
+        tfod = new TfodProcessor.Builder()
+                .setModelAssetName(TFOD_MODEL_ASSET)
+                .setModelLabels(LABELS)
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Amazona's_Webcam"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        builder.addProcessor(tfod);
+
+        visionPortal = builder.build();
+
+    }
+
+    private double telemetryTfod() {
+
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        if (currentRecognitions.size() == 0) return Double.MAX_VALUE;
+
+        Recognition recognition = currentRecognitions.get(0);
+
+        double x = (recognition.getLeft() + recognition.getRight()) / 2;
+        double y = (recognition.getTop() + recognition.getBottom()) / 2;
+
+        telemetry.addData("", " ");
+        telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+        telemetry.addData("- Position", "%.0f / %.0f", x, y);
+        telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+
+        return x;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startpose = new Pose2d(-20, -60, Math.toRadians(90));
+        drive.setPoseEstimate(startpose);
+        arm = new Arm(telemetry, hardwareMap);
+        claw = new Claw(hardwareMap, runtime);
+        drone = new Drone(hardwareMap);
+        subsystemManager.register(arm);
+        subsystemManager.register(claw);
+        subsystemManager.register(drone);
+
+        subsystemManager.init(telemetry);
+
+        initTfod();
+        telemetryTfod();
+        telemetry.update();
+
+        // Left trajectories
+        leftTraj0 = drive.trajectoryBuilder(startpose)
+                .forward(10)
+                .build();
+
+        leftTraj1 = drive.trajectoryBuilder(leftTraj0.end())
+                .lineToLinearHeading(new Pose2d(-8,-25, Math.toRadians(185)))
+                .build();
+
+        leftTraj2 = drive.trajectoryBuilder(leftTraj1.end())
+                .strafeRight(35)
+                .build();
+
+        leftTraj3 = drive.trajectoryBuilder(leftTraj2.end())
+                .lineToLinearHeading(new Pose2d(50,10, Math.toRadians(182)))
+                .build();
+
+        leftTraj4 = drive.trajectoryBuilder(leftTraj3.end())
+                .lineToLinearHeading(new Pose2d(40,-20, Math.toRadians(0)))
+                .build();
+
+        leftTraj5 = drive.trajectoryBuilder(leftTraj4.end())
+                .strafeLeft(30)
+                .build();
+
+        leftTraj6 = drive.trajectoryBuilder(leftTraj5.end())
+                .forward(13)
+                .build();
+
+
+        //Center trajectories
+        centerTraj0 = drive.trajectoryBuilder(startpose)
+                .forward(27)
+                .build();
+
+        centerTraj1 = drive.trajectoryBuilder(centerTraj0.end())
+                .strafeTo( new Vector2d(40,-27))
+                .build();
+
+        centerTraj2 = drive.trajectoryBuilder(centerTraj1.end())
+                .lineToLinearHeading(new Pose2d(45,-13, Math.toRadians(8)))
+                .build();
+
+        centerTraj3 = drive.trajectoryBuilder(centerTraj2.end())
+                .strafeLeft(30)
+                .build();
+
+        centerTraj4 = drive.trajectoryBuilder(centerTraj3.end())
+                .forward(10)
+                .build();
+
+
+        // Right trajectories
+        rightTraj0 = drive.trajectoryBuilder(startpose)
+                .forward(10)
+                .build();
+
+        rightTraj1 = drive.trajectoryBuilder(rightTraj0.end())
+                .lineToLinearHeading(new Pose2d(-35,-20, Math.toRadians(0)))
+                .build();
+
+        rightTraj2 = drive.trajectoryBuilder(rightTraj1.end())
+                .strafeLeft(25)
+                .build();
+
+        rightTraj3 = drive.trajectoryBuilder(rightTraj2.end())
+                .lineToLinearHeading(new Pose2d(40,-5, Math.toRadians(0)))
+                .build();
+
+        rightTraj4 = drive.trajectoryBuilder(rightTraj3.end())
+                .lineToLinearHeading(new Pose2d(45,-20, Math.toRadians(355)))
+                .build();
+
+        rightTraj5 = drive.trajectoryBuilder(rightTraj4.end())
+                .strafeLeft(25)
+                .build();
+
+        rightTraj6 = drive.trajectoryBuilder(rightTraj5.end())
+                .forward(15)
+                .build();
+
+
+        waitForStart();
+
+        telemetryTfod();
+        telemetry.update();
+
+        sleep(20);
+
+
+        if (telemetryTfod() > 0 && telemetryTfod() < 200) {
+            drive.followTrajectory(leftTraj0);
+            claw.groundPos();
+            drive.followTrajectory(leftTraj1);
+            claw.dropPixelSM();
+            sleep(2000);
+            claw.restPosition();
+            drive.followTrajectory(leftTraj2);
+            drive.followTrajectory(leftTraj3);
+            drive.followTrajectory(leftTraj4);
+            drive.followTrajectory(leftTraj5);
+            drive.followTrajectory(leftTraj6);
+        } else if (telemetryTfod() > 200 && telemetryTfod() < 500) {
+            claw.groundPos();
+            drive.followTrajectory(centerTraj0);
+            claw.dropPixelSM();
+            sleep(2000);
+            claw.restPosition();
+            drive.followTrajectory(centerTraj1);
+            drive.followTrajectory(centerTraj2);
+            drive.followTrajectory(centerTraj3);
+            drive.followTrajectory(centerTraj4);
+        } else if (telemetryTfod() > 500 && telemetryTfod() < 700){
+            drive.followTrajectory(rightTraj0);
+            claw.groundPos();
+            drive.followTrajectory(rightTraj1);
+            claw.dropPixelSM();
+            sleep(2000);
+            claw.restPosition();
+            drive.followTrajectory(rightTraj2);
+            drive.followTrajectory(rightTraj3);
+            drive.followTrajectory(rightTraj4);
+            drive.followTrajectory(rightTraj5);
+            drive.followTrajectory(rightTraj6);
+        }
+
+        PoseStorage.currentPose = drive.getPoseEstimate();
+
+    }
+}
